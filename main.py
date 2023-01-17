466e961eb01200472c4163a07e8ab4008f0c2eb3
jquave <jquave@gmail.com>
2023-01-17 12:17:07 -0600

    bloom

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from transformers import AutoTokenizer, BloomModel
+from transformers import AutoTokenizer, BloomModel, BloomForCausalLM
 import torch
 
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
-model = BloomModel.from_pretrained("bigscience/bloom-560m")
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom")
+model = BloomForCausalLM.from_pretrained("bigscience/bloom").cuda()
 
-inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-outputs = model(**inputs)
 
-last_hidden_states = outputs.last_hidden_state
+prompt = """
+Introduce yourself.
+"""
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids.cuda()
+gen_tokens = model.generate(
+    input_ids,
+    do_sample=True,
+    temperature=0.9,
+    max_length=len(prompt)+128,
+    )
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
+print(gen_text)
--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # This assumes pytorch/pytorch docker container
 pip install transformers
+git clone https://github.com/jquave/BLOOM-Startup.git
+python main.py
